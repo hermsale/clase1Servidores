d4cef286ef4e6830f90c9d0317f615ea51e04a54
hermsale <hermsale@gmail.com>
2020-11-03 21:57:49 -0300

    clase 1 v2

--- app.js
+++ app.js
@@ -10,29 +10,43 @@
 
 
 class Figuras{
-    constructor(){
-        this.alto = 60;
-        this.ancho = 40;
-        this.color = "verde";
+    constructor(alto, ancho, color){
+        this.alto = alto;
+        this.ancho = ancho;
+        this.color = color;
     }
-    tomarAncho(){
-        return (this.ancho); // retorna el valor del ancho 
+    tomarAncho(base){ // recibe el parametro 
+        return (this.ancho*base); // retorna el valor del ancho 
+    }
+
+    tomarColor(){
+        return (this.color);
     }
 
     //////////////// logica del objeto ////////////////
     // get es para resolver la logica de nuestro objeto 
-    get duplicarAncho(){
-        let fijarAncho = this.ancho *2;
-        return (fijarAncho);
-    }
+    // get duplicarAncho(){
+    //     let fijarAncho = this.ancho *2;
+    //     return (fijarAncho);
+    // }
 }
 
 
 // tenemos que instanciar al objeto para usarlo 
-const figuras = new Figuras();
+const figuras = new Figuras(60,40,"azul");
 
 // una vez que se instancea, se pide cosas 
 function tomarAncho(){
     console.log(figuras.ancho);
 }
 
+// le paso el parametro de la multiplicacion del ancho
+console.log(figuras.tomarAncho(2));
+console.log(figuras.tomarColor()); 
+
+/////// creo otra instancia de Figuras 
+const rectangulo = new Figuras(10,50,"verde");
+
+// le paso el parametro de la multiplicacion del ancho
+console.log(rectangulo.tomarAncho(4));
+console.log(rectangulo.tomarColor());
